ada8be88fa3d09cf03adb2e2cb0f7e284373421e
Shivansh-UD <shivanshgupta917@gmail.com>
2025-04-08 00:16:49 -0400

    added progress bars for both quizzes

--- src/Components/basicquizpage.tsx
+++ src/Components/basicquizpage.tsx
@@ -1,4 +1,6 @@
 import './basicquizpage.css';
+import React, { useState } from 'react';
+
 
 const bQuestions = [
   {
@@ -32,6 +34,30 @@ const bQuestions = [
 ];
 
 export function BasicQuiz(): React.JSX.Element {
+  const [answeredQuestions, setAnsweredQuestions]=useState<string[]>(Array(bQuestions.length).fill(""));//this starts with a array of empty strings and it has as many empty strings as the length of bQuestions
+
+  function optionSelect(qIndex: number, option: string) { //takes in the index of the question that is being answerd and the option choice selected for that question
+    const updatedAnswers = [...answeredQuestions]; //creates a new copy of answeredQuestions from above (state)
+    updatedAnswers[qIndex] = option;  //KEY: sets the option selected for a specific question to that questions position on the array using the qIndex. yaha pe ye basically uss copied array mei ja raha hai uss position pe jo question hai aur uska answer(option) ko set kar raha hai at that index.
+    setAnsweredQuestions(updatedAnswers); //finally here we are updating the original array with the answer choice finalized  
+  }
+  
+  //this functions checks/counts all the question that have been answered and then based on this we can calculate the progress 
+  //iterate over the array and if the question is NOT NULL("") increment the count by 1
+  function notNullAnswers():number{
+    let count:number=0;
+    for(let i=0;i<answeredQuestions.length;i++){
+      if(answeredQuestions[i]!==""){
+        count+=1;
+      }
+    }
+    return count;
+  }
+
+  const numOfAnswers=notNullAnswers();//calling the function to get count
+  const quizProgress=(numOfAnswers/bQuestions.length)*100;//this is used to check what percent of the quiz is completed and how much is still left to do 
+
+
   return (
     <div className="BTitle">
       <h1>Welcome to the Basic Quiz</h1>
@@ -48,6 +74,7 @@ export function BasicQuiz(): React.JSX.Element {
                     type="radio"
                     name={`question-${qIndex}`}
                     value={option}
+                    onChange={() => optionSelect(qIndex, option)}
                   />
                   {option}
                 </label>
@@ -63,6 +90,10 @@ export function BasicQuiz(): React.JSX.Element {
 
       <div className="Bbar">
         <h2>Progress Bar:</h2>
+        <div className="progress-container">{/*the outer div is like the white box that appears and it starts t get filled in with color as the quiz progresses*/}
+          <div className="progress-bar" style={{ height: `${quizProgress}%` }}></div>{/*this is the div that fills up the outer white box. in here we mention the height of the bar is based upon the variable we definded above to calculate the quiz progress*/}
+        </div>
+
       </div>
     </div>
   );
